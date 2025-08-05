333daaa6f4325bbc5c061221d0b90a641c2f1c51
Tejaswi <tejaswimadastu10@gmail.com>
2025-08-05 18:40:09 +0530

    Initial push after fixing submodule error

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+const toggleBtn = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
